0ffb6676b0f45a3007347beafb770b7373f72fdb
Stavros Thalassinos <40219645+stavthal@users.noreply.github.com>
2024-02-22 16:56:15 +0200

    Master (#1)
    * Initial commit
    
    * Setup the project
    
    * Updated readme

--- app.py
+++ app.py
@@ -1,13 +1,43 @@
 from flask import Flask
 from mail import send_mail
+from send_gmail import send_gmail
 
 app = Flask(__name__)
 
+# An array of emails (strings seperated by comma) to send out your emails.
+emails = ['<EMAIL>', '<EMAIL>']
 
-@app.route('/')
-def index():  # put application's code here
-    return send_mail()
+# Filepaths for html templates
+welcome_email_path = 'static/Welcome email (only registration) - RO.html'
+welcome_email_direct_verification_level_1_path = 'static/Welcome email direct verification level 1 - RO.html'
+reminder_email_for_verification_path = 'static/Reminder email for verification - RO.html'
+confirmation_level_2_path= 'static/Confirmation of verification (level2) - RO.html'
+confirmation_level_1_path = 'static/Confirmation of verification (level1) - RO.html'
 
 
+# Defined example routes for sending out the mails
+@app.route('/send-welcome-email')
+def index():
+    return send_gmail(emails, 'Test Welcome Email', welcome_email_path )
+@app.route('/send-welcome-email-direct-verification')
+def direct_verification():
+    return send_gmail(emails, 'Test Welcome Email (Direct Verification Level 1)',
+                     welcome_email_direct_verification_level_1_path)
+
+@app.route('/send-reminder-email')
+def reminder_email():
+    return send_gmail(emails, 'Test Reminder Email', reminder_email_for_verification_path)
+
+@app.route('/send-confirmation-email-level-1')
+def confirmation_level_1():
+    return send_gmail(emails, 'Test Confirmation Level 1', confirmation_level_1_path)
+
+@app.route('/send-confirmation-email-level-2')
+def confirmation_level_2():
+    return send_gmail(emails, 'Test Confirmation Level 2', confirmation_level_2_path)
+
+
+# You can always toggle the debug to off
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
+
--- mail.py
+++ mail.py
@@ -2,21 +2,35 @@ import os
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
 
-def send_mail():
+def send_mail(to_emails, subject, html_file_path):
     sendgrid_api_key = os.getenv('SENDGRID_API_KEY')
+    # Ensure to_emails is a list of strings
+    if isinstance(to_emails, str):
+        to_emails = [to_emails]  # Convert a single email address to a list
+
+    # Read HTML content from the file
+    try:
+        with open(html_file_path, 'r', encoding='utf-8') as file:
+            html_content = file.read()
+    except Exception as e:
+        print(f"Failed to read HTML file: {e}")
+        return f"Failed to send email: {e}"
+
     if sendgrid_api_key is None:
         print("SENDGRID_API_KEY environment variable is not set.")
     else:
         message = Mail(
             from_email='stavros8003@gmail.com',
-            to_emails='stavros8003@gmail.com',
-            subject='Sending with Twilio SendGrid is Fun',
-            html_content='<strong>and easy to do anywhere, even with Python</strong>')
+            to_emails=to_emails,
+            subject=subject,
+            html_content=html_content)
         try:
             sg = SendGridAPIClient(sendgrid_api_key)
             response = sg.send(message)
             print(response.status_code)
             print(response.body)
             print(response.headers)
+            return "Email sent successfully"
         except Exception as e:
             print(str(e))
+            return "An error occurred while sending email"
